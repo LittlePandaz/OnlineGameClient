ff6d43ca6b5c9f643680b882603867400e609b02
LittlePandaz <34212099+fabrice.mereu@gmail.com>
2018-02-26 10:31:15 +0100

    Base project

---
+++ OnlineGame/OnlineGame/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineGame.Models
+{
+    public class Player
+    {
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
